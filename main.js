ce620b623233d061a00be2ce89d4c93d888de078
Sujith Surendranath <sujit.surendranath@hashedin.com>
2019-03-05 14:35:44 +0530

    Display full screen and height changes

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
+let win
 function createWindow () {
   // Create the browser window.
-  let win = new BrowserWindow({
+  win = new BrowserWindow({
       webPreferences:{
           webviewTag:true
       }
   })
-
+  win.maximize();
   // and load the index.html of the app.
-  win.loadFile('dashboard.html')
+  win.loadFile('dashboard.html');
+  win.on('closed', () => {
+    win = null
+  })
 }
+
 
 app.on('ready', createWindow)
