61c112b7d5869034cef787b1461a043064658300
Sujith Surendranath <sujit.surendranath@hashedin.com>
2019-03-05 16:37:45 +0530

    apps, hangout, notification disabled

--- assests/js/dashboard.js
+++ assests/js/dashboard.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { app, BrowserWindow } = require('electron');
 
 const gmailButton = document.querySelector('#gmail-button');
 const chatButton = document.querySelector('#chat-button');
@@ -6,32 +7,48 @@ const googleButton = document.querySelector('#google-button');
 const gitlabButton = document.querySelector('#gitlab-button');
 const webviewContainer = document.querySelector('#webview-container');
 const webviewElement = document.querySelector('.webview');
-console.log(webviewElement)
+const loading = document.querySelector('#loading');
+
+// Loading animation
+const intialWebViewLoad = () => {
+    loading.style.display='block'
+    webviewElement.style.display = "none";
+}
 
 gmailButton.addEventListener('click', (e) => {
-    webviewElement.setAttribute('src','https://mail.google.com/mail/u/0/');
-    webviewElement.setAttribute('id','gmailWebView');
-    webviewElement.setAttribute('preload',path.join(__dirname,'preloads','gmail','gmailCompose.js'));
+    intialWebViewLoad();
+    webviewElement.setAttribute('src', 'https://mail.google.com/mail/u/0/');
+    webviewElement.setAttribute('id', 'gmailWebView');
+    webviewElement.setAttribute('preload', path.join(__dirname, 'preloads', 'gmail', 'gmailCompose.js'));
 })
 
 chatButton.addEventListener('click', (e) => {
-    webviewElement.setAttribute('src','https://chat.google.com/u/0/');
-    webviewElement.setAttribute('id','chatWebView');
+    intialWebViewLoad();
+    webviewElement.setAttribute('src', 'https://chat.google.com/u/0/');
+    webviewElement.setAttribute('id', 'chatWebView');
 })
 
 googleButton.addEventListener('click', (e) => {
-    webviewElement.setAttribute('src','https://www.google.com');
-    webviewElement.setAttribute('id','googleWebView');
+    intialWebViewLoad();
+    webviewElement.setAttribute('src', 'https://www.google.com');
+    webviewElement.setAttribute('id', 'googleWebView');
 })
 
 gitlabButton.addEventListener('click', (e) => {
-    webviewElement.setAttribute('src','https://git.hashedin.com');
-    webviewElement.setAttribute('id','gitlabWebView');
+    intialWebViewLoad();
+    webviewElement.setAttribute('src', 'https://git.hashedin.com');
+    webviewElement.setAttribute('id', 'gitlabWebView');
+})
+
+
+webviewElement.addEventListener('did-finish-load', () => {
+    console.log(loading.style);
+    loading.style.display='none'
+    webviewElement.style.display = "inline-flex"
 })
 
 
 // Development
 webviewElement.addEventListener('dom-ready', () => {
     webviewElement.openDevTools()
-    })
-    
+})
